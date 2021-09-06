9028f115c5f28709b928f5eb90d2ea7c8f1ac230
saitan <satya@gmail.com>
2021-09-06 20:58:05 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Responsive and Animated Portfolio Website & App - Flutter UI
+
+**Packages we are using:**
+
+- flutter_svg: [link](https://pub.dev/packages/flutter_svg)
+- goole_fonts: [link](https://pub.dev/packages/google_fonts)
+- animated_text_kit: [link](https://pub.dev/packages/animated_text_kit)
+
+**Fonts**
+
+- Poppins [link](https://fonts.google.com/specimen/Poppins)
+
+It is a responsive developer portfolio website and app using flutter. I also add animation which works perfectly on mobile and the web. 
+
+### Responsive and Animated Portfolio Website & App Final UI
+
+![Preview](/gif.gif)
+
+![App UI](/ui.png)
---
+++ lib/models/Project.dart
@@ -0,0 +1,39 @@
+class Project {
+  final String? title, description;
+
+  Project({this.title, this.description});
+}
+
+List<Project> demo_projects = [
+  Project(
+    title: "Welcome page, Login Page and Sign up page - Flutter UI",
+    description:
+        "The Login page I made, is a nice clean onboaring screen for different apps which has a nice email, phone number authentication and GoogleSignin a nice clean onboarding screen for your e-commerce app that can run both Andriod and iOS devices because it builds with flutter. Then on the second episode, we build a Sign in, Forgot Password screen with a custom error indicator.",
+  ),
+  Project(
+    title:
+        "E-Commerce Complate App with Firebase login and backend- Flutter UI",
+    description:
+        "The e-commerce app I made, will fetch data from api and will show the products it has a real time cart system  with firebase authentication of email, phone number and google sigin, you can view products nice clean onboarding screen for your e-commerce app that can run both Andriod and iOS devices because it builds with flutter. Then on the second episode, we build a Sign in, Forgot Password screen with a custom error indicator.",
+  ),
+  Project(
+    title: "Music Player App with Jio savaan Api - Flutter UI",
+    description:
+        "This app was made because i was tired of listening to ads, This app gets most of the songs, with options to download the songs and view the lyrics it is responsive it can be run on web, desktop, phone and tablet . In this flutter responsive video, we will show you the real power of flutter. Make mobile, web, and desktop app from a single codebase.",
+  ),
+  Project(
+    title: "Chat/Messaging App Light and Dark Theme - Flutter UI",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  ),
+  Project(
+    title: "Responsive Admin Panel or Dashboard - Flutter UI",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  ),
+  Project(
+    title: "Covid-19 App - Flutter UI",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  ),
+];
